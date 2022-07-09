af76039f84581ecb9f5ba7ff4d9fd8923b0ed695
kang <rilac1@naver.com>
2022-07-10 04:09:03 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/forrestGoF/JobScanner/JobScannerApplication.java
@@ -0,0 +1,13 @@
+package com.forrestGoF.JobScanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobScannerApplication.class, args);
+	}
+
+}
