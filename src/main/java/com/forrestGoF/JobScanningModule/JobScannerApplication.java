e96a5a764a742856cadf241211ec899c4837237e
kang <rilac1@naver.com>
2022-07-11 16:18:17 +0900

    :sparkles: GCP 라이브러리 작성

--- src/main/java/com/forrestGoF/JobScanningModule/JobScannerApplication.java
+++ src/main/java/com/forrestGoF/JobScanningModule/JobScannerApplication.java
@@ -1,4 +1,4 @@
-package com.forrestGoF.JobScanner;
+package com.forrestGoF.JobScanningModule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
