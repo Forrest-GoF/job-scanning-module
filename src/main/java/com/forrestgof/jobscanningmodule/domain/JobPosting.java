7fbc33b2f85739d799a4ba57ffa0ee36f8c7b329
kang <rilac1@naver.com>
2022-07-17 04:44:03 +0900

    :art: JobPosting 주식별자 분리 / `Company`, `Location` 연관관계 추가 / `Salary`: [엔티티]->[Embedded 타입]

--- src/main/java/com/forrestgof/jobscanningmodule/domain/JobPosting.java
+++ src/main/java/com/forrestgof/jobscanningmodule/domain/JobPosting.java
@@ -2,35 +2,37 @@ package com.forrestgof.jobscanningmodule.domain;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "job")
-@Builder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class JobPosting {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "job_id")
-	private String id;
+	private Long id;
+
+	@Column(name = "external_key", unique = true)
+	private String key;
 
 	@Column(name = "job_title")
 	private String title;
@@ -59,8 +61,22 @@ public class JobPosting {
 	@Column(name = "job_description")
 	private String description;
 
-	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JoinColumn(name = "salary_id")
+	@Embedded
 	private Salary salary;
 
+	@Builder
+	public JobPosting(String key, String title, Company company, Location location, String platform,
+		LocalDateTime postedAt, JobType jobType, String applyingUrl, String description,
+		Salary salary) {
+		this.key = key;
+		this.title = title;
+		this.company = company;
+		this.location = location;
+		this.platform = platform;
+		this.postedAt = postedAt;
+		this.jobType = jobType;
+		this.applyingUrl = applyingUrl;
+		this.description = description;
+		this.salary = salary;
+	}
 }
