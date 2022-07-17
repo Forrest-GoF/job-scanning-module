cb9f9c26bac2f8777d5002856d714e72a030c687
Juwon Park <devprofessionalism@gmail.com>
2022-07-17 20:48:35 +0900

    :white_check_mark: SerpJobSearchLibraryService 구현

---
+++ src/main/java/com/forrestgof/jobscanningmodule/service/serpapi/SerpJobSearchLibraryService.java
@@ -0,0 +1,74 @@
+package com.forrestgof.jobscanningmodule.service.serpapi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.forrestgof.jobscanningmodule.dto.SearchFilter;
+import com.forrestgof.jobscanningmodule.serpapi.GoogleSearch;
+import com.forrestgof.jobscanningmodule.serpapi.config.SerpApiKey;
+import com.google.gson.JsonObject;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SerpJobSearchLibraryService {
+
+	private final SerpApiKey serpApiKey = SerpApiKey.getInstance();
+
+	// JsonObject search(SearchFilter searchFilter) {
+	// 	Map<String, String> parameter = new HashMap<>();
+	//
+	// 	parameter.put("api_key", serpApiKey.getKey());
+	// 	parameter.put("engine", "google_jobs");
+	// 	parameter.put("q", searchFilter.getQ());
+	//
+	// 	parameter.put("location", searchFilter.getLocation());
+	// 	parameter.put("uule", searchFilter.getUule());
+	// 	parameter.put("google_domain", searchFilter.getGoogleDomain());
+	// 	parameter.put("gl", searchFilter.getGl());
+	// 	parameter.put("hl", searchFilter.getHl());
+	// 	parameter.put("start", searchFilter.getStart());
+	// 	parameter.put("chips", searchFilter.getChips());
+	// 	parameter.put("irad", searchFilter.getIrad());
+	//
+	// 	GoogleSearch googleSearch = new GoogleSearch(parameter);
+	// 	JsonObject jobSearchResult = null;
+	// 	try {
+	// 		jobSearchResult = googleSearch.getJson();
+	// 	} catch (Exception e) {
+	// 		System.out.println(e.getMessage());
+	// 	}
+	//
+	// 	return jobSearchResult;
+	// }
+
+	JsonObject search(SearchFilter searchFilter) {
+		Map<String, String> parameter = new HashMap<>();
+
+		parameter.put("api_key", serpApiKey.getKey());
+		parameter.put("engine", "google_jobs");
+		parameter.put("q", searchFilter.getQ());
+
+		parameter.put("location", searchFilter.getLocation());
+		parameter.put("uule", searchFilter.getUule());
+		parameter.put("google_domain", searchFilter.getGoogleDomain());
+		parameter.put("gl", searchFilter.getGl());
+		parameter.put("hl", searchFilter.getHl());
+		parameter.put("start", searchFilter.getStart());
+		parameter.put("chips", searchFilter.getChips());
+		parameter.put("irad", searchFilter.getIrad());
+
+		GoogleSearch googleSearch = new GoogleSearch(parameter);
+		JsonObject jobSearchResult = null;
+		try {
+			jobSearchResult = googleSearch.getJson();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return jobSearchResult;
+	}
+}
